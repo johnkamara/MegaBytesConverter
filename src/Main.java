d6b8356b0d48fbd14cad86c27457cb586cfe1343
johnkamara <john.kamara.ubuntuafrika@gmail.com>
2021-03-11 03:20:15 +0000

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args){
+        int mega = MegaBytesConverter.printMegaBytesAndKiloBytes(1024);
+        System.out.println("mega =" + mega);
+        MegaBytesConverter.printMegaBytesAndKiloBytes(1024);
+
+    }
+}
---
+++ src/MegaBytesConverter.java
@@ -0,0 +1,14 @@
+public class MegaBytesConverter {
+
+    public static int printMegaBytesAndKiloBytes(int kiloBytes){
+        if (kiloBytes <0){
+            System.out.println("Invalid Value");
+        }else {
+            int megaBytes = (kiloBytes/1024);
+            int kiloRemainder = kiloBytes%1024;
+            System.out.println(kiloBytes + " KB = " + megaBytes + " MB and " + kiloRemainder + " KB");
+        }
+
+        return kiloBytes;
+    }
+}
